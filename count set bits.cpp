38038439f3b63c370ddce9cf3228ef469d434c8b
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-03-04 16:56:38 +0530

    Update count set bits.cpp

--- count set bits.cpp
+++ count set bits.cpp
@@ -1,4 +1,6 @@
- int n;
+//  right most set bit ko nikalo o(1) mai phir n mai se minus kr do toh n ki right most set bit 0 ho jayegi phir continue kr jb tk n = 0 nhi ho jata 
+
+int n;
  cin>>n;
  int count =0;
  while(n !=0){
